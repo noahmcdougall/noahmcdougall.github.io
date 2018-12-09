564958b559ee085a1b15d0c5b2584148b35eea78
Noah McDougall <noahmcdougall@gmail.com>
2018-12-08 22:55:18 -0600

    Geoscience Mainpage Scroller
    Added rock layers and updated code for scrolling parallax.  Commented
    out some code that I'm attempting to use to make the button disappear
    and reappear on other side.  Will work on that later.

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -2,11 +2,11 @@
 $(document).ready(function() {
 	//run test to see if page is in @media mode
 	checkSize();
-	
+
 	//run test on resize of window
 	$(window).resize(checkSize);
 	var displaytype = $('.media-check').css('display');
-	
+
 	//Header scrolling
 	smoothScroll(1000);
 });
@@ -32,24 +32,30 @@ function checkSize() {
 
 // Geoscience Section
 function geoscienceScroll() {
-	
+
 	// Defines the foreground scrolling
 	var geoforeScroll = $(window).scrollTop();
-	if ((-40 + geoforeScroll / 6) < 0){
-		$('.dynamicbackground').css('background-position','calc(50% + 100px) calc(50% + '+ (40 - geoforeScroll / 6) +'px)');
-	} else {
-		$('.dynamicbackground').css('background-position','calc(50% + 100px) calc(50% - '+ (-40 + geoforeScroll / 6) +'px)');
+	var $maxgeoScroll=590;
+	if($(window).scrollTop()>$maxgeoScroll) geoforeScroll=$maxgeoScroll;
+	if (geoforeScroll > 150) {
+		$('.dynamicbackground').css('background-position','calc(50%) calc(50% - '+ (-195 + geoforeScroll / 1.5) +'px)');
 	}
 
 	//Defines text column scrolling
 	var geotextScroll = $(window).scrollTop();
 	$('.geotextscroll').css('margin', 'calc(-50px - '+ (geotextScroll / 8) +'px 10px');
-	
-	
+
+	//Visibility flipper
+	//if($(window).scrollTop()>250) {
+	//	$('.col-1').addClass('makeinvisible');
+	//}
+
 	//Defines the background scrolling
 	var geobackScroll = $(window).scrollTop();
-	$('.staticbackground').css('background-position','center calc(50% - '+ (geobackScroll / 13) +'px)');
-	
+	if($(window).scrollTop()>$maxgeoScroll) geobackScroll=$maxgeoScroll;
+	if (geobackScroll >150) {
+		$('.staticbackground').css('background-position','center calc(50% - '+ (geobackScroll / 6) +'px)');
+	}
 }
 
 // Caving Section
@@ -63,9 +69,9 @@ function cavingScroll() {
 
 function smoothScroll (duration) {
 	$('a[href^="#"]').on('click', function(event) {
-		
+
 		var target = $( $(this).attr('href') );
-		
+
 		if( target.length ) {
 				event.preventDefault();
 				$('html, body').animate({
